56b2151dceea973686f6bed0c9c477deb03722bb
paoascanio <131598223+paoascanio@users.noreply.github.com>
2025-07-09 10:23:26 -0400

    fix se termino de arreglar los query handlers

--- ClaimsMS/ClaimsMS.Application/Claim/Handler/Queries/GetClaimByStatusFilteredQueryHandler.cs
+++ ClaimsMS/ClaimsMS.Application/Claim/Handler/Queries/GetClaimByStatusFilteredQueryHandler.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using MediatR;
+using ClaimsMS.Application.Claim.Queries;
+using ClaimsMS.Common.Dtos.Claim.Response;
+using ClaimsMS.Core.Repositories;
+using ClaimsMS.Infrastructure.Exceptions;
+using ClaimsMS.Core.Repositories.Claims;
+
 
 namespace ClaimsMS.Application.Claim.Handler.Queries
 {
-    internal class GetClaimByStatusFilteredQueryHandler
-    {
+
+        public class GetClaimByStatusFilteredQueryHandler : IRequestHandler<GetClaimByStatusFilteredQuery, List<GetClaimDto>>
+        {
+            private readonly IClaimRepositoryMongo _claimRepository;
+            private readonly IMapper _mapper;
+
+            public GetClaimByStatusFilteredQueryHandler(IClaimRepositoryMongo claimRepository, IMapper mapper)
+            {
+            _claimRepository = claimRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<List<GetClaimDto>> Handle(GetClaimByStatusFilteredQuery request, CancellationToken cancellationToken)
+            {
+
+                if (request.UserId == Guid.Empty)
+                {
+                    throw new ArgumentException("El ID de usuario es requerido para filtrar reclamos.");
+                }
+
+
+                var claims = await _claimRepository.GetByStatusClaimsAsync(
+                    request.UserId,
+                    request.auctionId,
+                    request.status
+                   
+                );
+
+
+                if (claims == null || !claims.Any())
+                {
+                    return new List<GetClaimDto>();
+                }
+
+                var claimsDtoList = _mapper.Map<List<GetClaimDto>>(claims);
+
+                return claimsDtoList;
+            }
+        }
     }
-}
--- ClaimsMS/ClaimsMS.Common/obj/Debug/net8.0/ClaimsMS.Common.AssemblyInfo.cs
+++ ClaimsMS/ClaimsMS.Common/obj/Debug/net8.0/ClaimsMS.Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClaimsMS.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ba625f00864e8100cc2f922d4ae575964310d18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b499db178cf1f2dc56b542c7fa6ac00e73215e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClaimsMS.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClaimsMS.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
